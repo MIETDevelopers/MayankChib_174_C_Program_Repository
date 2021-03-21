6493104d78d950ed756494c3778914c89fe6a188
unknown <2020a1r174@mietjammu.in>
2021-03-21 16:06:23 +0530

    Factorial using for loop

--- 10_Factorial.c
+++ 10_Factorial.c
@@ -1,12 +1,18 @@
-#include<stdio.h>  
-int main()    
-{    
- int i,fact=1,number;    
- printf("Enter a number: ");    
-  scanf("%d",&number);    
-    for(i=1;i<=number;i++){    
-      fact=fact*i;    
-  }    
-  printf("Factorial of %d is: %d",number,fact);    
-return 0;  
-}   
+//Name: Mayank Chib
+//Program: To find factorial using for loop
+
+#include<stdio.h>   //Pre-process directive to include standard input and output functions header file 
+
+int main()     //Main function
+{
+  int i,f=1,num;            //variable declaration using int datatype
+ 
+  printf("Enter an integer");   //printf function calling
+  scanf("%d",&num);      //scanf function calling to receive input
+ 
+  for(i=1;i<=num;i++)  //for loop started 
+      f=f*i;
+ 
+  printf("Factorial of %d is: %d",num,f);  //print statement should be according to Question
+  return 0; //return function
+}
