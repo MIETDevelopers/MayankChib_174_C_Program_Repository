7e188216aca408526015ae8b7fff0db9b3bf624d
unknown <2020a1r174@mietjammu.in>
2021-03-21 15:25:31 +0530

    To find out area of Circle, Rectangle and Square based on user choice

--- 9_Area.c
+++ 9_Area.c
@@ -1,30 +1,33 @@
-#include <stdio.h>
+//Name: Mayank Chib
+//Program Purpose: To find out area of Circle, Rectangle and Square based on user choice
+
+#include <stdio.h>        //Pre-process directive to include standard input and output functions header file
 void main ()
 {
       int choice, radius, length ,base, breadth, height;
       float area;
-      printf("Input 1 for area of circle\n");
-      printf("Input 2 for area of rectangle\n");
-      printf("Input 3 for area of triangle\n");
-      printf("Input your choice : ");
-      scanf("%d",&choice);
-      switch(choice)
+      printf("Input 1 for area of circle\n");     //printf function calling
+      printf("Input 2 for area of rectangle\n");     //printf function calling
+      printf("Input 3 for area of triangle\n");     //printf function calling
+      printf("Input your choice : ");     //printf function calling
+      scanf("%d",&choice);    //scanf function calling
+      switch(choice)           //using switch to select between shapes
       {
            case 1:
-                 printf("Input radious of the circle : ");
-                 scanf("%d",&radius);
+                 printf("Input radious of the circle : ");     //printf function calling
+                 scanf("%d",&radius);   //scanf function calling
                  area=3.14* radius* radius;
                  break;
             case 2:
-                  printf("Input length and width of the rectangle : ");
-                  scanf("%d%d", &length, &breadth);
+                  printf("Input length and width of the rectangle : ");     //printf function calling
+                  scanf("%d%d", &length, &breadth);   //scanf function calling
                   area=length*breadth;
                   break;
             case 3:
-                  printf("Input the base and hight of the triangle :");
-                  scanf("%d%d", &base, &height);
+                  printf("Input the base and hight of the triangle :");     //printf function calling
+                  scanf("%d%d", &base, &height);      //scanf function calling
                   area=.5*base*height;
                   break;
           }
-          printf("The area is : %f\n",area);
-}
+          printf("The area is : %f\n",area);     //printf function calling
+        }
