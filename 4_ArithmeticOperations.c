edf5ac7391f73b709eb4928fd7aa5d8bdb13324b
unknown <2020a1r174@mietjammu.in>
2021-03-21 14:54:35 +0530

    Arithmetic operations using addition, subtraction, multiplication and remainder operator

--- 4_ArithmeticOperations.c
+++ 4_ArithmeticOperations.c
@@ -1,18 +1,29 @@
-#include <stdio.h>
-int main() {
+//Name: Mayank Chib 
+//Program: Understanding different arithmetic operators using a simple calclator
+#include <stdio.h>     //Pre-process directive to include standard input and output functions header file
 
-	int num1, num2, sum, subtraction, multiplication, remainder;
-	scanf("%d %d", &num1, &num2 );
-
-	sum=num1+num2;
-	printf("The sum of two number=%d\n", sum);
-
-	subtraction=num1-num2;
-	printf("The subtraction of two number=%d\n", subtraction);
-
-	multiplication=num1*num2;
-	printf("The multiplication of two number=%d\n", multiplication);
-
-	remainder=num1%num2;
-	printf("The remainder of two number=%d\n", remainder);
+int main() {                 //main fucntion
+    char operator;             //variable declaraion with data type char
+    double n1, n2;               //variable declaration with datatype double 
+    printf("Enter an operator (+, -, *, /): ");  //printf funtion to print operator
+    scanf("%c", &operator);               //scanf function to read the opperator from the user
+    printf("Enter two operands: ");
+    scanf("%lf %lf",&n1, &n2);
+    switch(operator)                     //declaration of switch case operator
+    {
+        case '+':                                          //printf function calling for addition 
+            printf("%.1lf + %.1lf = %.1lf",n1, n2, n1+n2);        
+            break; // this statement is correct syntaxt wise
+        case '-':                                                   //printf function calling for subtraction
+            printf("%.1lf - %.1lf = %.1lf",n1, n2, n1-n2);
+        case '*':                                                           //printf function calling for multiplication
+            printf("%.1lf * %.3lf = %.2lf",n1, n2, n1*n2);
+        case '/':                                                          //printf function calling for division
+            printf("%.1lf / %.1lf = %.1lf",n1, n2, n1/n2);
+            break;
+        // operator doesn't match any case constant +, -, *, /
+        default:
+            printf("Error! operator is not correct");
+    }
+    return 0;                                       //return function
 }
