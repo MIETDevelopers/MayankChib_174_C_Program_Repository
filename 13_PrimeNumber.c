7803e3e4d30fec983c13c78a456493b9ab593987
unknown <2020a1r174@mietjammu.in>
2021-03-21 16:23:29 +0530

    Check if a number is prime or not using while loop

--- 13_PrimeNumber.c
+++ 13_PrimeNumber.c
@@ -1,26 +1,25 @@
-#include <stdio.h>
-int main() {
-  int n, i, flag = 0;
-  printf("Enter a positive integer: ");
-  scanf("%d", &n);
+//Name: Mayank Chib
+//Program: Program to check whether the number is prime or not
 
-  for (i = 2; i <= n / 2; ++i) {
-    // condition for non-prime
-    if (n % i == 0) {
-      flag = 1;
-      break;
+#include <stdio.h>         //Pre-process directive to include standard input and output functions header file
+int main(){ //main function
+     int n, i, count = 0;
+
+        printf("Enter the Number.\n"); //printf function calling
+        scanf("%d",&n);//This will scan (read) the number entered by user.
+
+        for(i=2; i<=n/2; ++i){ //Declaration of a forloop
+    //Check for non prime number 
+        if(n%i==0){
+            count=1;
+            break;
+        }
     }
-  }
-
-  if (n == 1) {
-    printf("1 is neither prime nor composite.");
-  } 
-  else {
-    if (flag == 0)
-      printf("%d is a prime number.", n);
+    //Declaration of if else statement
+    if (count==0)
+        printf("%d is a Prime number.",n);
     else
-      printf("%d is not a prime number.", n);
-  }
+        printf("%d is not a Prime number.",n);
+    return 0; //return function
 
-  return 0;
-}
+} 
