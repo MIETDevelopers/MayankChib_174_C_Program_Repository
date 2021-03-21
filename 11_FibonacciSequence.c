4edb3904c7ebe1e358e37ab0ed2d98beddf61e05
unknown <2020a1r174@mietjammu.in>
2021-03-21 16:09:26 +0530

    Fibonacci sequence using for loop

--- 11_FibonacciSequence.c
+++ 11_FibonacciSequence.c
@@ -1,26 +1,19 @@
-#include<stdio.h>
- 
-int main()
-{
-   int n, first = 0, second = 1, next, c;
- 
-   printf("Enter the number of terms\n");
-   scanf("%d",&n);
- 
-   printf("First %d terms of Fibonacci series are :-\n",n);
- 
-   for ( c = 0 ; c < n ; c++ )
-   {
-      if ( c <= 1 )
-         next = c;
-      else
-      {
-         next = first + second;
-         first = second;
-         second = next;
-      }
-      printf("%d\n",next);
-   }
- 
-   return 0;
-}
+//Name: Mayank Chib
+//Program: Fibonacci series using for loop
+#include<stdio.h>     //Pre-process directive to include standard input and output functions header file 
+
+int main()   //Main function 
+{    
+ int n1=0,n2=1,n3,i,number;    
+ printf("Enter the number of elements:");   //Ask user to input number of terms   
+ scanf("%d",&number);    
+ printf("\n%d %d",n1,n2);//printing 0 and 1    
+ for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  printf(" %d",n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+  return 0;  // return 0 to oprerating system
+ }    
