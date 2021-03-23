8696ccbdfc1d71bf57c284db739425b0c700e987
unknown <2020a1r174@mietjammu.in>
2021-03-23 20:36:19 +0530

    To demonstrate the use of various string operations

---
+++ 21_String.c
@@ -0,0 +1,20 @@
+//Name: Mayank Chib
+//To demonstrate the use of various string operations
+
+#include <stdio.h> //Pre-Process directive to include standard input output header file
+#include <string.h> //Pre-Process directive to include string input output header file
+int main()
+{
+    /* String Declaration*/
+    char nickname[20];
+
+    /* Console display using puts */
+    puts("Enter your Nick name:");
+
+    /*Input using gets*/
+    gets(nickname);
+
+    puts(nickname);
+
+    return 0;
+}
