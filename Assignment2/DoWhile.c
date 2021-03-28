7e315b3b03608537a5d51462a8ffdf52be81fce2
unknown <2020a1r174@mietjammu.in>
2021-03-28 16:28:47 +0530

    Do While Program

---
+++ Assignment2/DoWhile.c
@@ -0,0 +1,22 @@
+//Name: Mayank Chib
+//Program:
+
+#include<stdio.h>   //Pre-process directive to include standard input and output functions header file 
+
+int main()     //Main function
+{
+    double number, sum = 0;
+
+    // the body of the loop is executed at least once
+    do
+    {
+        printf("Enter a number: "); //printf function calling
+        scanf("%lf", &number);  //scanf function calling
+        sum += number;
+    }
+    while(number != 0.0);
+
+    printf("Sum = %.2lf",sum);  //printf function calling
+
+    return 0;//return function
+}
