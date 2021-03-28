511c173af3ce6f4bc5b08dc328a18d82c07c4a6e
unknown <2020a1r174@mietjammu.in>
2021-03-28 16:23:19 +0530

    Check no. is armstrong or not using while loop

--- 15_Armstrong.c
+++ 15_Armstrong.c
@@ -4,20 +4,20 @@
 #include <stdio.h>         //Pre-process directive to include standard input and output functions header file
 int main()                 //Main function
 {
-int n,num,r,ans=0;    // variable declaration
-printf("Enter a positive integer: ");    //Ask user to input positive number  
-scanf("%d", &num);
-n=num;
-while(n>0)                 /* Loop to calculate the sum of the cubes of its digits */
-{
-r=n%10;
-ans=ans+r*r*r;
-n=n/10;
-}
+	int n,num,r,ans=0;    // variable declaration
+	printf("Enter a positive integer: ");    //Ask user to input positive number  
+	scanf("%d", &num);
+	n=num;
+	while(n>0)                 /* Loop to calculate the sum of the cubes of its digits */
+	{
+		r=n%10;
+		ans=ans+r*r*r;
+		n=n/10;
+	}
  
-if(ans==num)               /* if else condition to print Armstrong or Not */
-printf("%d is an Armstrong number.",num);
-else
-printf("%d is not an Armstrong number.",num);
-return 0;   // return 0 to oprerating system
+	if(ans==num)               /* if else condition to print Armstrong or Not */
+		printf("%d is an Armstrong number.",num);
+	else
+		printf("%d is not an Armstrong number.",num);
+	return 0;   // return 0 to oprerating system
 }
--- test_Hello.c
+++ test_Hello.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
-int main() {
-	printf("Hello World!");
+ #include <stdio.h>
+#include <stdlib.h>
+int main (){
+	int a[2][2]={1,2,3,4};
+	printf("%d", a[1][1]);
 	return 0;
 }
